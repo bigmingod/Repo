dc119d085a0090d331a34b031db28a68a938e5df
bigmingod <35945592+bigmingod@users.noreply.github.com>
2020-07-31 13:51:27 +0800

    Update goinit.sh

--- goinit.sh
+++ goinit.sh
@@ -2,5 +2,7 @@ sudo wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
 sudo tar -C /usr/local/ -xzvf go1.14.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 sudo wget https://raw.githubusercontent.com/bigmingod/Repo/master/webserver.go
+/usr/local/go/bin/go
+echo done
 /usr/local/go/bin/go build webserver.go
 nohup ./webserver &
