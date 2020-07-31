35cb7ab459f199711a291bceadc88439a43b2c5a
bigmingod <35945592+bigmingod@users.noreply.github.com>
2020-07-31 17:16:49 +0800

    Update goinit.sh

--- goinit.sh
+++ goinit.sh
@@ -1,6 +1,7 @@
 sudo wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
 sudo tar -C /usr/local/ -xzvf go1.14.6.linux-amd64.tar.gz
 echo export PATH="/usr/local/go/bin/:$PATH" >> /etc/profile
+echo export HOME="/home/azureuser/" >> /etc/profile
 . /etc/profile
 sudo wget https://raw.githubusercontent.com/bigmingod/Repo/master/webserver.go
 go build webserver.go
