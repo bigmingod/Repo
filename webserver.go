699f5a342e32c0f31b3f29c102fd81b5d9dd58d5
bigmingod <35945592+bigmingod@users.noreply.github.com>
2020-08-04 14:50:13 +0800

    Update webserver.go

--- webserver.go
+++ webserver.go
@@ -1,35 +1,51 @@
 package main
+
 import (
-    "fmt"
-    "io"
-    "io/ioutil"
-    "net/http"
-    "time"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request){
-    //打印请求主机地址
-    fmt.Println(time.Now().String())
-    fmt.Println(r.Host)
-    //打印请求头信息
-    fmt.Printf("header content:[%v]\n", r.Header)
+var start_time int64
+var flag int
 
-    //获取 post 请求中 form 里边的数据
-    fmt.Printf("form content:[%s, %s]\n", r.PostFormValue("username"), r.PostFormValue("passwd"))
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	//打印请求主机地址
+	fmt.Println(time.Now().String())
+	fmt.Println(r.Host)
+	//打印请求头信息
+	fmt.Printf("header content:[%v]\n", r.Header)
 
-    //读取请求体信息
-    bodyContent, err := ioutil.ReadAll(r.Body)
-    if err != nil && err != io.EOF {
-        fmt.Printf("read body content failed, err:[%s]\n", err.Error())
-        return
-    }
-    fmt.Printf("body content:[%s]\n", string(bodyContent))
+	//获取 post 请求中 form 里边的数据
+	fmt.Printf("form content:[%s, %s]\n", r.PostFormValue("username"), r.PostFormValue("passwd"))
 
-    //返回响应内容
-    fmt.Fprintf(w, "hello world ~")
+	//读取请求体信息
+	bodyContent, err := ioutil.ReadAll(r.Body)
+	if err != nil && err != io.EOF {
+		fmt.Printf("read body content failed, err:[%s]\n", err.Error())
+		return
+	}
+	fmt.Printf("body content:[%s]\n", string(bodyContent))
+	//返回响应内容
+	var nowtime int64
+	nowtime = time.Now().Unix()
+	if nowtime-start_time < 1800 {
+		fmt.Fprintf(w, strconv.FormatInt(time.Now().Unix(), 10))
+	} else if flag < 1 {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "fail")
+		flag++
+	} else {
+		fmt.Fprintf(w, strconv.FormatInt(time.Now().Unix(), 10))
+	}
 }
 
-func main (){
-    http.HandleFunc("/", IndexHandler)
-    http.ListenAndServe(":8002", nil)
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	start_time = time.Now().Unix()
+	flag = 0
+	http.ListenAndServe(":8002", nil)
 }
