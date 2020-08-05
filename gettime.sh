573f4be4ae9fbeb23220a725b551cf9cac60de25
bigmingod <35945592+bigmingod@users.noreply.github.com>
2020-08-05 20:30:58 +0800

    Create gettime.sh

---
+++ gettime.sh
@@ -0,0 +1,5 @@
+current=`date "+%Y-%m-%d %H:%M:%S"`  
+timeStamp=`date -d "$current" +%s`   
+#将current转换为时间戳，精确到毫秒  
+currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
+echo $currentTimeStamp
