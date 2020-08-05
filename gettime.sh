2a3b58d8ca8f7e26e691c763e815aeec6451aabe
bigmingod <35945592+bigmingod@users.noreply.github.com>
2020-08-05 21:39:13 +0800

    Update gettime.sh

--- gettime.sh
+++ gettime.sh
@@ -1,6 +1,6 @@
 current=`date "+%Y-%m-%d %H:%M:%S"`  
 timeStamp=`date -d "$current" +%s`     
-currentTimeStamp=$(((timeStamp*1000+`date "+%N"`/1000000)/1000))
+currentTimeStamp=$((timeStamp+`date "+%N"`/1000))
 echo $currentTimeStamp
 
 sudo wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
