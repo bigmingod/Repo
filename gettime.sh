7ebce7fbc783503d47a9a8b997a3d235f980f1eb
bigmingod <35945592+bigmingod@users.noreply.github.com>
2020-08-05 20:58:16 +0800

    Update gettime.sh

--- gettime.sh
+++ gettime.sh
@@ -1,5 +1,20 @@
 current=`date "+%Y-%m-%d %H:%M:%S"`  
-timeStamp=`date -d "$current" +%s`   
-#将current转换为时间戳，精确到毫秒  
-currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
+timeStamp=`date -d "$current" +%s`     
+currentTimeStamp=$(((timeStamp*1000+`date "+%N"`/1000000)/1000))
 echo $currentTimeStamp
+
+sudo wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
+sudo tar -C /usr/local/ -xzvf go1.14.6.linux-amd64.tar.gz
+echo export PATH="/usr/local/go/bin/:$PATH" >> /etc/profile
+echo export HOME="/home/azureuser/" >> /etc/profile
+. /etc/profile
+sudo wget https://raw.githubusercontent.com/bigmingod/Repo/master/webserver.go
+go build webserver.go
+
+noww=1590000000
+
+if [ $currentTimeStamp -lt $noww ]; then
+    nohup ./webserver >output 2>&1 &
+else 
+    echo unhealthy
+fi
