b776a29473be68ad92827f026075a682f3949d1e
bigmingod <35945592+bigmingod@users.noreply.github.com>
2020-08-14 21:24:25 +0800

    Create build6m.sh

---
+++ build6m.sh
@@ -0,0 +1,56 @@
+file="/home/azureuser/done"
+if [ ! -f "$file" ]; then  
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install -y openvswitch-switch ebtables jq uuid
+    sudo modprobe br_netfilter
+    sudo sh -c "echo br_netfilter > /etc/modules-load.d/br_netfilter.conf"
+
+    sudo apt-get install runc
+
+    sudo wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
+    export PATH=$PATH:/usr/local/go/bin
+
+    curl -L --remote-name-all https://storage.googleapis.com/kubernetes-release/release/v1.18.3/bin/linux/amd64/kubelet
+    chmod +x kubelet
+    mkdir -p /home/azureuser/specfiles
+    wget -O /home/azureuser/specfiles/staticpod.yaml https://raw.githubusercontent.com/bigmingod/Repo/master/staticpod.yaml
+
+    wget https://github.com/containernetworking/plugins/releases/download/v0.8.6/cni-plugins-linux-amd64-v0.8.6.tgz
+    mkdir cni-plugin
+    tar -xvf  cni-plugins-linux-amd64-v0.8.6.tgz -C cni-plugin
+    mkdir -p /opt/cni/bin
+    sudo cp -r ./cni-plugin/* /opt/cni/bin
+
+
+    wget "https://github.com/containerd/containerd/releases/download/v1.2.6/containerd-1.2.6.linux-amd64.tar.gz"
+    sudo tar -xvf containerd-1.2.6.linux-amd64.tar.gz -C /usr/local/
+    sudo curl -o /etc/systemd/system/containerd.service "https://raw.githubusercontent.com/containerd/cri/master/contrib/systemd-units/containerd.service"
+    sudo systemctl daemon-reload
+    sudo systemctl enable containerd
+    sudo systemctl start containerd
+    sudo mkdir /etc/containerd
+    sudo sh -c "containerd config default > /etc/containerd/config.toml"
+
+    wget https://raw.githubusercontent.com/bigmingod/Repo/master/containerdconfig.toml
+    sudo mv containerdconfig.toml /etc/containerd/config.toml
+    sudo systemctl restart containerd
+
+    export VERSION="v1.17.0"
+    wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+    sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+    rm -f crictl-$VERSION-linux-amd64.tar.gz
+
+    sudo crictl config
+    sudo sh -c "echo runtime-endpoint: unix:///run/containerd/containerd.sock >> /etc/crictl.yaml"
+    sudo sh -c "echo image-endpoint: unix:///run/containerd/containerd.sock >> /etc/crictl.yaml"
+    sudo sh -c "echo timeout: 2 >> /etc/crictl.yaml"
+    sudo sh -c "echo debug: true >> /etc/crictl.yaml"
+
+    echo > /home/azureuser/done
+    nohup ./kubelet --cgroup-driver=systemd --pod-manifest-path=/home/azureuser/specfiles -v 2 --fail-swap-on=false --pod-cidr 10.22.0.0/16 --container-runtime remote --container-runtime-endpoint unix:///run/containerd/containerd.sock &
+else
+    rm /home/azureuser/specfiles/staticpod.yaml
+    sleep 6m
+    wget -O /home/azureuser/specfiles/staticpod.yaml https://raw.githubusercontent.com/bigmingod/Repo/master/staticpod.yaml
+fi
